ba637ff1ada834ab7bce4d5100ec037be114aeed
Daniele Oliveira <droliver.dev@gmail.com>
2021-03-31 15:08:24 -0300

    paginação

--- src/content/Projects.svelte
+++ src/content/Projects.svelte
@@ -4,29 +4,33 @@
     repositoryStore,
     initialState,
     topics,
+    pagination
   } from "../stores/repositories";
 
   let repositories = [];
   repositoryStore.subscribe((_) => (repositories = _.repositories));
   const resetRepo = () => repositoryStore.set({ ...initialState });
   let filter = "";
+
   const fetchRepo = async () => {
-    let searchUrl = `https://api.github.com/search/repositories?q=topic:codesolidario`;
+    let searchUrl = `https://api.github.com/search/repositories?q=topic:opensource`;
     if (filter) {
       searchUrl += `+topic:${filter}`;
     }
-    searchUrl += `&type=Repositories&ref=searchresults`;
+    searchUrl += `&type=Repositories&ref=searchresults&page=${$pagination.page}&per_page=10`;
     try {
       const repositories = await axios
         .get(searchUrl)
         .then((response) => {
+          $pagination.totalCount = response.data.total_count;
+          $pagination.totalPages = Math.ceil($pagination.totalCount / $pagination.totalPerPage)
           return response.data.items;
         })
         .catch((err) => {
           throw new Error(err);
         });
       repositoryStore.set({ ...initialState, repositories });
-      console.log(repositories);
+      console.log($pagination);
       return repositories;
     } catch (error) {
       resetRepo();
@@ -84,6 +88,22 @@
       <p style="color: red">{error.message}</p>
     {/await}
   </div>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <button class="page-link" aria-label="Previous" on:click={$pagination.page -= 1, handleSelect} disabled={$pagination.page <= 1}>
+          <span aria-hidden="true">&laquo;</span>
+          <span class="sr-only">Previous</span>
+        </button>
+      </li>
+      <li class="page-item">
+        <button class="page-link" aria-label="Next" on:click={$pagination.page += 1, handleSelect} disabled={$pagination.page >= $pagination.totalPages}>
+          <span aria-hidden="true">&raquo;</span>
+          <span class="sr-only">Next</span>
+        </button>
+      </li>
+    </ul>
+  </nav>
 </section>
 <style>
   .repos {
--- src/stores/repositories.js
+++ src/stores/repositories.js
@@ -5,6 +5,13 @@ export const initialState = {
     repositories: []
 }
 
+export const pagination = writable({
+    page: 1,
+    totalPages: 1,
+    totalCount: 1,
+    totalPerPage: 10
+})
+
 export const topics = writable([
     {name:"emprego"}, {name: "ambiental"}, {name:"educacao"}, {name:"seguranca"}, {name:"saude"}, {name: "saneamento"}, {name:"animais"}
 ])
